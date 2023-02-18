32c6594ee5009d51a711c4f14e339ad7650c584b
José Vitor Maia <121828716+JoseVitorMaia@users.noreply.github.com>
2023-02-18 12:00:56 -0300

    Melhoria de Layout e variaveis

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let horario = document.getElementsByName('horario')
 let etapa = document.getElementsByName('etapa')
 let uso = document.getElementById('uso')
 let participantes = document.getElementById('parcicipantes')
@@ -7,49 +8,72 @@ let participativos = document.getElementsByName('participativos')
 let problema = document.getElementsByName('problema')
 let nota = document.getElementsByName('nota')
 let consideracoes = document.getElementById('consideracoes')
-
-
-let teste1 = document.getElementById('1')
-let teste2 = document.getElementById('2')
-let teste3 = document.getElementById('3')
-let teste4 = document.getElementById('4')
-let teste5 = document.getElementById('5')
-let teste6 = document.getElementById('6')
-let teste7 = document.getElementById('7')
-let teste8 = document.getElementById('8')
-let teste9 = document.getElementById('9')
-
-let textoPronto = document.getElementById('textoPronto')
+let final = document.getElementById('final')
 
 
 function gerar(){
+    final.innerHTML = '';
+
     for (var i = 0 in etapa)
         if (etapa[i].checked)
-            teste1.innerHTML = 'Qual etapa do treinamento: ' + etapa[i].value
+        final.innerHTML += '- Qual etapa da consultoria: ' + etapa[i].value + "<br>"
 
-    teste2.innerHTML = 'Porcentagem de uso: ' + uso.value + '%'
+        final.innerHTML += '- Porcentagem de uso: ' + uso.value + '%' + "<br>"
 
-    teste3.innerHTML = 'Quem participou do treinamento: ' + participantes.value
+        final.innerHTML += '- Quem participou da consultoria: ' + participantes.value + "<br>"
+
+    for (var i = 0 in horario)
+        if (horario[i].checked)
+        final.innerHTML += '- Atrasaram para a consultoria: ' + horario[i].value + "<br>"
 
     for (var i = 0 in atividades)
         if (atividades[i].checked)
-            teste4.innerHTML = 'Realizaram as atividades anteriores? ' + atividades[i].value
+        final.innerHTML += '- Realizaram as atividades anteriores: ' + atividades[i].value + "<br>"
     
     for (var i = 0 in duvidas)
         if (duvidas[i].checked)
-            teste5.innerHTML = 'Dúvidas/Dificuldades da etapa anterior? ' +  duvidas[i].value
+        final.innerHTML += '- Dúvidas/Dificuldades da etapa anterior: ' +  duvidas[i].value + "<br>"
 
         for (var i = 0 in participativos)
             if (participativos[i].checked)
-                teste6.innerHTML = 'Participativos? ' + participativos[i].value
+            final.innerHTML += '- Participativos: ' + participativos[i].value + "<br>"
 
     for (var i = 0 in problema)
         if (problema[i].checked)
-            teste7.innerHTML = 'Algum problema? ' + problema[i].value
+        final.innerHTML += '- Algum problema: ' + problema[i].value + "<br>"
 
     for (var i = 0 in nota)
         if (nota[i].checked)
-            teste8.innerHTML = 'Nota para a consultoria: '  + nota[i].value        
+        final.innerHTML += '- Nota para a consultoria: '  + nota[i].value + "<br>"     
     
-    teste9.innerHTML = 'Considerações adicionais: ' + consideracoes.value       
+        final.innerHTML += '- Considerações adicionais: ' + consideracoes.value + "<br>"
+
+    final.style.backgroundColor = "#c7b184f3"    
 }
+
+
+
+/*
+function copiarTexto(){
+    let textoCopiado = document.getElementById('final');
+    textoCopiado.select();
+    textoCopiado.setSelectionRange(0,99999);
+    document.execCommand("copy")
+}
+
+
+
+
+ let copyText = document.getElementById('respostas');
+    let input = document.createElement("input");
+    input.id = "inp";
+    input.style.whiteSpace="pre-line"
+    input.value = copyText.outerText;
+    copyText.appendChild(input);
+
+    let copy = document.getElementById('inp');
+    copy.select();
+    document.execCommand("Copy");
+    alert("O texto copiado foi: " + copy.value);   
+    copyText.removeChild(input);
+    */
